1d5aa2fa4c604fecd9506707a06f209505dfe504
SaiSaraswathi2006 <147473340+SaiSaraswathi2006@users.noreply.github.com>
2023-10-10 11:22:14 +0530

    Add files via upload

---
+++ area of the reactangle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main ()
+{
+	int area,length,breadth;
+	printf("enter the value of length");
+	scanf("%d",& length);
+	printf("enter the value of breadth");
+	scanf("%d",& breadth);
+	area=length*breadth;
+	printf("area of the rectangle=%d",area);
+}
